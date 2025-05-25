b82ba8a34b760a2a6399922c2b9ed9ade771d229
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-25 11:19:59 +0530

    create employee and project model

---
+++ demo/src/main/java/fas/ict/demo/model/Employee.java
@@ -0,0 +1,22 @@
+package fas.ict.demo.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee extends Person{
+    @Id
+	private String EmpNo;
+	private String name;
+	private int age;
+	private double salary;
+	private String gender;
+	@ManyToOne
+	private Department department;
+	@ManyToMany(mappedBy = "employees")
+	private List<Project>projects;
+}
