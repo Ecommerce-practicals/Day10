a0f359888ee82812550880a3b5a04a87f51e5dcf
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-25 13:49:18 +0530

    Update DepartmentService.java

--- demo/src/main/java/fas/ict/demo/service/DepartmentService.java
+++ demo/src/main/java/fas/ict/demo/service/DepartmentService.java
@@ -1,5 +1,34 @@
 package fas.ict.demo.service;
 
-public class DepartmentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fas.ict.demo.model.Department;
+import fas.ict.demo.repo.DepartmentRepository;
+import jakarta.persistence.EntityNotFoundException;
+@Service
+public class DepartmentService {
+    @Autowired
+	private DepartmentRepository repo;
+	
+	public List<Department> getDepts(){
+		return repo.findAll();
+	}
+	public Department getDept( int id) {
+		if(repo.findById(id).isEmpty()) {
+			throw new EntityNotFoundException("Department Not Found");
+		}
+		return repo.findById(id).get();
+	}
+	
+	public String addDept(Department department) {
+		if(repo.findById(department.getId()).isPresent()) {
+			throw new DuplicateKeyException("The department id is already available");
+		}
+		repo.save(department);
+		return "new department added";
+	}
+	
 }
