a45f3fb304cff09a3ec401882dbe86fa4acfd6ef
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-25 13:54:14 +0530

    creating exceptionhandler

--- demo/src/main/java/fas/ict/demo/service/DepartmentService.java
+++ demo/src/main/java/fas/ict/demo/service/DepartmentService.java
@@ -3,6 +3,7 @@ package fas.ict.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import fas.ict.demo.model.Department;
