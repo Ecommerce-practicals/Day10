1a91b617cd61e046ef2fe36fcbfdf153a8ababf4
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-25 11:09:19 +0530

    create person.java

---
+++ demo/src/main/java/fas/ict/demo/model/Person.java
@@ -0,0 +1,10 @@
+package fas.ict.demo.model;
+
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person {
+    private String name;
+	private int age;
+	private String gender;
+}
