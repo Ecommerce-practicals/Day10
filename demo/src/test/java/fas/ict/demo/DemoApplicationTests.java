5ed5b9d541ca1e44147568009742364f2134c420
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-25 10:10:25 +0530

    initialize project

---
+++ demo/src/test/java/fas/ict/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fas.ict.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
